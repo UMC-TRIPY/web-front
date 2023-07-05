5300796a8f42cda7279e716c2c0d56c25d974b42
rbgksqkr <rbgks1937@naver.com>
2023-07-06 03:16:23 +0900

    Fix: #1 - svg 파일 클래스명 오류 수정

--- src/components/community/Dropdown.tsx
+++ src/components/community/Dropdown.tsx
@@ -8,10 +8,6 @@ const Dropdown = (props: any) => {
             <button
                 className='flex items-center whitespace-nowrap rounded bg-neutral-50 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-800 shadow-[0_4px_9px_-4px_#fbfbfb] transition duration-150 ease-in-out hover:bg-neutral-100 hover:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.3),0_4px_18px_0_rgba(251,251,251,0.2)] focus:bg-neutral-100 focus:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.3),0_4px_18px_0_rgba(251,251,251,0.2)] focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.3),0_4px_18px_0_rgba(251,251,251,0.2)] motion-reduce:transition-none'
                 type='button'
-                id='dropdownMenuButton9'
-                data-te-dropdown-toggle-ref
-                aria-expanded='false'
-                data-te-ripple-init
             >
                 {category}
                 <span className='ml-2 w-2'>
@@ -22,9 +18,9 @@ const Dropdown = (props: any) => {
                         className='h-5 w-5'
                     >
                         <path
-                            fill-rule='evenodd'
+                            fillRule='evenodd'
                             d='M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z'
-                            clip-rule='evenodd'
+                            clipRule='evenodd'
                         />
                     </svg>
                 </span>
