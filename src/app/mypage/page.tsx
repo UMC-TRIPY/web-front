efcf101040f58070ce855ac53b8bba8768f98b5d
seungboshim <fodo9898@gmail.com>
2023-07-11 01:38:34 +0900

    Feat : #13 - 탭바 컴포넌트 구현

---
+++ src/app/mypage/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import MypageMenus from "@/components/mypage/mypageMenus";
+
+export default function mypage () {
+    return (
+        <div>
+            <MypageMenus />
+        </div>
+    )
+}
