91d2ba2747723b31c676e8b851f01ac1b243389e
Seungbo Shim <112371013+seungboshim@users.noreply.github.com>
2023-07-10 17:22:54 +0900

    [FEAT] #8 - 헤더 및 푸터 구현 (#10)
    * Feat : #8 - 헤더, 푸터 구현 및 사용할 이미지 삽입
    
    * Feat : #8 - layout.tsx에 병합 후 확인
    
    * Feat : #8 - 로그인, 로그아웃 및 메뉴바 선택 구현
    
    * Feat : #8 - 코드 다듬기 작업
    
    * Feat : #8 - 헤더 메뉴바 선택시 아이콘 위치 조정

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,109 @@
+'use client';
+
+import Link from "next/link";
+import React, { useState } from "react";
+
+interface MenuProps {
+    menu: string;
+    select: boolean;
+    index: number;
+    onClick: (index: number) => void;
+}
+
+export default function Header () {
+    // 로그인, 로그아웃 상태구현
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const handleLogin = () => {
+        setIsLoggedIn(true);
+    };
+
+    const handleLogout = () => {
+        setIsLoggedIn(false);
+    }
+
+    // 메뉴바 선택항목 상태구현
+    const [menus, setMenus] = useState<[string, boolean][]>([
+        ['여행정보', false],
+        ['일정관리', false],
+        ['여행가방', false],
+        ['커뮤니티', false],
+        ['이용안내', false],
+    ]);
+
+    const Menu = ({ menu, select, index, onClick }: MenuProps) => {
+        return (
+            <div className='relative flex flex-col items-center'>
+                {select && (
+                    <img
+                        className='absolute top-0 transform -translate-y-full'
+                        src='/images/selected.png'
+                        alt='none'
+                    />
+                )}
+                <span
+                    className={
+                        select
+                            ? 'font-bold hover:cursor-pointer'
+                            : 'hover:cursor-pointer'
+                    }
+                    onClick={() => onClick(index)}
+                >
+                    {menu}
+                </span>
+            </div>
+        );
+    };
+
+    const menuClick = (selectedIndex: number) => {
+        const updatedMenus: [string, boolean][] = menus.map((menu, index) => [
+            menu[0],
+            index === selectedIndex
+        ]);
+        setMenus(updatedMenus);
+    };
+
+    return (
+        <header className='x-0 top-0 z-50 left-0 w-full bg-white border-b border-gray-300'>
+            <div className='container mx-auto my-0.5 h-24 flex justify-between flex-wrap p-5'>
+                <div className="flex space-x-16">
+                    <Link href='/' className="flex">
+                        <img className='w-120' src='/images/logo1.svg' alt="트리피 로고" />
+                    </Link>
+                    <nav className="flex items-center space-x-8">
+                        {menus.map((menu, index) => (
+                            <Menu
+                                key={`menu${index}`}
+                                menu={menu[0]}
+                                select={menu[1]}
+                                index={index}
+                                onClick={menuClick}
+                            />
+                        ))}
+                    </nav>
+                </div>
+                <div className="flex items-center space-x-6">
+                    {isLoggedIn ? (
+                        <>
+                            <Link href='/mypage'>마이페이지</Link>
+                            <button onClick={handleLogout} className="focus:outline-none">
+                                로그아웃
+                            </button>
+                            <Link href='/'>
+                                <img className='w-12 h-12' src='/images/user.svg' alt="프로필 사진" />
+                            </Link>
+                        </>
+                    ) : (
+                        <>
+                        
+                            <button onClick={handleLogin} className="focus:outline-none">
+                                로그인
+                            </button>
+                            <Link href='/'>회원가입</Link>
+                        </>
+                    )}
+                </div>
+            </div>
+        </header>
+    );
+}
