6411d28f376daa63ea5a5053a8e52a0cc374bfcb
gs0428 <lider28@naver.com>
2023-07-12 20:54:54 +0900

    Feat : #18 - 여행 일정 관리 페이지 구현

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,3 +1,11 @@
+'use client';
+
+import DetailSchedule from '@/components/detailschedule/DetailSchedule';
+
 export default function schedule() {
-    return <h1>일정관리 페이지입니다.</h1>;
+    return (
+        <div>
+            <DetailSchedule />
+        </div>
+    );
 }
