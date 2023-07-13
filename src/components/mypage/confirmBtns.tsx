f7f962933bbefee73409823ebf45de629e348372
seungboshim <fodo9898@gmail.com>
2023-07-13 19:54:52 +0900

    Feat : #26 - 버튼 컴포넌트 분리

--- src/components/mypage/confirmBtns.tsx
+++ src/components/mypage/confirmBtns.tsx
@@ -1,29 +1,9 @@
-import React from 'react';
-
-interface ButtonProps {
-    label: string;
-    color: string;
-    onClick?: () => void;
-}
-
-function Button (props:ButtonProps) {
-    return (
-        <div className="m-2 mt-14 mb-48">
-            <button
-                className={`${props.color} px-11 py-3.5 rounded-md`}
-                onClick={props.onClick}
-            >
-                {props.label}
-            </button>
-        </div>
-    )
-}
-
+import ConfirmBtn from "../layout/confirmBtn"
 export default function ConfirmBtns () {
     return (
         <div className="flex justify-center">
-            <Button label="탈퇴하기" color="bg-lightgrey" />
-            <Button label="수정하기" color="bg-primary" />
+            <ConfirmBtn label="탈퇴하기" color="bg-lightgrey" />
+            <ConfirmBtn label="수정하기" color="bg-primary" />
         </div>  
     )
 }
