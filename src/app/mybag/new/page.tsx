c85467b0239d7bb2774914c1d700d89fad9e0576
rbgksqkr <rbgks1937@naver.com>
2023-07-29 15:06:16 +0900

    Feat: #49 - 상단탭 컴포넌트 분리

--- src/app/mybag/new/page.tsx
+++ src/app/mybag/new/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import OtherSchedule from '@/components/detailschedule/OtherSchedule';
+import TopTab from '@/components/mybag/TopTab';
 import Image from 'next/image';
 import React, { useState } from 'react';
 
 const NewBag = () => {
-    const [topTab, setTopTab] = useState([
+    const [tabs, setTabs] = useState([
         {
             id: '0',
             text: '내 가방 만들기',
@@ -19,53 +20,19 @@ const NewBag = () => {
     ]);
 
     const handleClickTab = (e: any) => {
-        setTopTab(
-            topTab.map((tab) =>
+        setTabs(
+            tabs.map((tab) =>
                 tab.id === e.target.id
                     ? { ...tab, clicked: true }
                     : { ...tab, clicked: false }
             )
         );
     };
+
     return (
         <>
             <div className='flex items-end h-36 gap-4 text-grey mb-4'>
-                {topTab.map((tab) => (
-                    <div
-                        key={tab.id}
-                        id={tab.id}
-                        className={
-                            tab.clicked
-                                ? 'text-main-color cursor-pointer'
-                                : 'text-grey cursor-pointer'
-                        }
-                        onClick={handleClickTab}
-                    >
-                        <div className='flex justify-center'>
-                            {tab.clicked && (
-                                <Image
-                                    src='/images/selected.png'
-                                    alt=''
-                                    width={15}
-                                    height={15}
-                                />
-                            )}
-                        </div>
-
-                        {tab.text}
-                    </div>
-                ))}
-
-                {/* <div
-                    id='0'
-                    className='text-main-color'
-                    onClick={handleClickTab}
-                >
-                    내 가방 만들기
-                </div>
-                <div id='1' onClick={handleClickTab}>
-                    폴더보기
-                </div> */}
+                <TopTab tabs={tabs} handleClickTab={handleClickTab} />
             </div>
             <OtherSchedule />
         </>
