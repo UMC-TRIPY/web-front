8775eca954a2851ddef0e8da15925444dffb10ce
rbgksqkr <rbgks1937@naver.com>
2023-08-03 19:05:43 +0900

    Feat: #58 - 폴더보기 컴포넌트 분리

--- src/components/mybag/DirectoryList.tsx
+++ src/components/mybag/DirectoryList.tsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AiOutlinePicture } from 'react-icons/ai';
 import { GoFileDirectory } from 'react-icons/go';
 
 const DirectoryList = () => {
+    const [directories, setDirectories] = useState([
+        {
+            id: 0,
+            name: '갤러리',
+            image: <AiOutlinePicture size={200} className='text-main-color' />
+        },
+        {
+            id: 1,
+            name: '파일목록',
+            image: <GoFileDirectory size={200} className='text-main-color' />
+        }
+    ]);
+
     return (
         <>
-            <div className='flex justify-center items-center basis-[25%]'>
-                <div className='flex flex-col items-center gap-8 cursor-pointer'>
-                    <AiOutlinePicture size={200} className='text-main-color' />
-                    <div className='py-2'>갤러리</div>
+            {directories.map((directory) => (
+                <div
+                    key={directory.id}
+                    className='flex justify-center items-center basis-[25%]'
+                >
+                    <div className='flex flex-col items-center gap-8 cursor-pointer'>
+                        {directory.image}
+                        <div className='py-2'>{directory.name}</div>
+                    </div>
                 </div>
-            </div>
-            <div className='flex justify-center items-center basis-[25%]'>
-                <div className='flex flex-col items-center gap-8 cursor-pointer'>
-                    <GoFileDirectory size={200} className='text-main-color' />
-                    <div className='py-2'>파일목록</div>
-                </div>
-            </div>
+            ))}
         </>
     );
 };
