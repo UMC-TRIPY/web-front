fb3b46bc59483c7f7cc05064f447c09adfc21e6f
gs0428 <lider28@naver.com>
2023-08-09 15:08:01 +0900

    Feat : #103 - 페이지네이션 확장

--- src/components/mypagefriend/myFriends.tsx
+++ src/components/mypagefriend/myFriends.tsx
@@ -1,51 +1,47 @@
-import FriendTwoBtn from "./friendTwoBtn";
-import React, { useState } from "react";
+import Pagination from '../maincommunity/Pagination';
+import FriendTwoBtn from './friendTwoBtn';
+import React, { useState, useEffect } from 'react';
 
-function MyFriends () {
-    const [currentPage, setCurrentPage] = useState(1);
-    const [friendsPerPage] = useState(4);
-    const [friends, setFriends] = useState([
-        '미리',
-        '메이',
-        '규',
-        '루카',
-        '레니',
-        '시미',
-        '초이',
-        '폴',
-        '에그먼',
-    ]);
+const friends: string[] = [
+    '미리',
+    '메이',
+    '규',
+    '루카',
+    '레니',
+    '시미',
+    '초이',
+    '폴',
+    '에그먼'
+];
 
-    const handlePrevClick = () => {
-        setCurrentPage((prevPage) => prevPage - 1)
-    };
-    const handleNextClick = () => {
-        setCurrentPage((prevPage) => prevPage + 1)
-    };
-
-    const indexOfLastFriend = currentPage * friendsPerPage;
-    const indexOfFirstFriend = indexOfLastFriend - friendsPerPage;
-    const currentFriends = friends.slice(indexOfFirstFriend, indexOfLastFriend);
-
+function MyFriends() {
+    const totalPages = Math.ceil(friends.length / 4);
+    const [current, setCurrent] = useState<number>(1);
+    const [datas, setDatas] = useState<string[]>(friends.slice(0, 4));
+    useEffect(() => {
+        setDatas(friends.slice((current - 1) * 4, current * 4));
+    }, [current]);
     return (
         <div>
-            <div className="text-3xl font-bold mx-4 mt-20">나의 친구</div>
-            <div className="mx-4 py-4">
-                {currentFriends.map((friend, index) => (
-                    <FriendTwoBtn key={index} name={friend} label1="초대하기" label2="친구끊기" px={6} />
+            <div className='text-3xl font-bold mx-4 mt-20'>나의 친구</div>
+            <div className='mx-4 py-4'>
+                {datas.map((data: string, index: number) => (
+                    <FriendTwoBtn
+                        key={index}
+                        name={data}
+                        label1='초대하기'
+                        label2='친구끊기'
+                        px={6}
+                    />
                 ))}
             </div>
-            <div className="flex justify-center">
-                <button className="mx-4 px-2" onClick={handlePrevClick} disabled={currentPage === 1}>
-                    &lt;
-                </button>
-                {currentPage}
-                <button className="mx-4 px-2" onClick={handleNextClick} disabled={indexOfLastFriend >= friends.length}>
-                    &gt;
-                </button>
-            </div>
+            <Pagination
+                totalPages={totalPages}
+                current={current}
+                setCurrent={setCurrent}
+            />
         </div>
-    )
+    );
 }
 
 export default MyFriends;
--- src/components/schedulemain/myTravel.tsx
+++ src/components/schedulemain/myTravel.tsx
@@ -1,75 +1,102 @@
-import React, { useState } from "react";
-import RoundBtn from "../layout/roundBtn";
+import React, { useState, useEffect } from 'react';
+import RoundBtn from '../layout/roundBtn';
+import Pagination from '../maincommunity/Pagination';
 
-function MyTravel () {
-    const [currentPage, setCurrentPage] = useState(1);
-    const [travelsPerPage] = useState(8);
-    const [travels, setTravels] = useState<{id: number, dates: string, places: string}[]>([
-        {id: 1, dates: "2023.06.30~2023.07.02 (2박 3일)", places: "부산"},
-        {id: 2, dates: "2023.05.09~2023.05.11 (2박 3일)", places: "바르셀로나, 세비야"},
-        {id: 3, dates: "2023.05.09~2023.05.11 (2박 3일)", places: "파리, 런던"},
-        {id: 4, dates: "2023.05.09~2023.05.11 (2박 3일)", places: "오사카, 나라, 교토"},
-        {id: 5, dates: "2023.05.09~2023.05.11 (2박 3일)", places: "방콕"},
-        {id: 6, dates: "2023.05.09~2023.05.11 (2박 3일)", places: "싱가폴, 말레이시아"},
-        {id: 7, dates: "2023.05.09~2023.05.11 (2박 3일)", places: "제주도"},
-        {id: 8, dates: "2023.05.09~2023.05.11 (2박 3일)", places: "도쿄"},
-        {id: 9, dates: "2023.05.09~2023.05.11 (2박 3일)", places: "어디어디"},
-        {id: 10, dates: "언제언제", places: "어디어디"},
-        {id: 11, dates: "언제언제", places: "어디어디"},
-        {id: 12, dates: "언제언제", places: "어디어디"},
-        {id: 13, dates: "언제언제", places: "어디어디"},
-    ]);
-    const handlePrevClick = (): void => {
-        setCurrentPage((prevPage) => prevPage - 1)
-    };
-    const handleNextClick = (): void => {
-        setCurrentPage((prevPage) => prevPage + 1)
-    };
+interface Props {
+    id: number;
+    dates: string;
+    places: string;
+}
 
-    const indexOfLastTravel = currentPage * travelsPerPage;
-    const indexOfFirstTravel = indexOfLastTravel - travelsPerPage;
-    const currentTravels = travels.slice(indexOfFirstTravel, indexOfLastTravel) as { id: number; dates: string; places: string }[];
+const contents = [
+    { id: 1, dates: '2023.06.30~2023.07.02 (2박 3일)', places: '부산' },
+    {
+        id: 2,
+        dates: '2023.05.09~2023.05.11 (2박 3일)',
+        places: '바르셀로나, 세비야'
+    },
+    {
+        id: 3,
+        dates: '2023.05.09~2023.05.11 (2박 3일)',
+        places: '파리, 런던'
+    },
+    {
+        id: 4,
+        dates: '2023.05.09~2023.05.11 (2박 3일)',
+        places: '오사카, 나라, 교토'
+    },
+    { id: 5, dates: '2023.05.09~2023.05.11 (2박 3일)', places: '방콕' },
+    {
+        id: 6,
+        dates: '2023.05.09~2023.05.11 (2박 3일)',
+        places: '싱가폴, 말레이시아'
+    },
+    { id: 7, dates: '2023.05.09~2023.05.11 (2박 3일)', places: '제주도' },
+    { id: 8, dates: '2023.05.09~2023.05.11 (2박 3일)', places: '도쿄' },
+    { id: 9, dates: '2023.05.09~2023.05.11 (2박 3일)', places: '어디어디' },
+    { id: 10, dates: '언제언제', places: '어디어디' },
+    { id: 11, dates: '언제언제', places: '어디어디' },
+    { id: 12, dates: '언제언제', places: '어디어디' },
+    { id: 13, dates: '언제언제', places: '어디어디' }
+];
+
+function MyTravel() {
+    const totalPages = Math.ceil(contents.length / 8);
+    const [current, setCurrent] = useState<number>(1);
+    const [datas, setDatas] = useState<Props[]>(contents.slice(0, 8));
+    useEffect(() => {
+        setDatas(contents.slice((current - 1) * 8, current * 8));
+    }, [current]);
     return (
-        <div className="mx-4 mt-16">
-            <div className="text-3xl font-bold mb-5">
-                내 여행 목록
-            </div>
-            <div className="rounded-md bg-brightgrey">
-                <div className="border-b border-b-lightgrey py-5">
-                    <div className="flex justify-between">
-                        <div className="w-1/3 text-center"><p>일정</p></div>
-                        <div className="w-1/3 text-center"><p>장소</p></div>
-                        <div className="w-1/3 text-center"><p>관리하기</p></div>
-                    </div>    
-                </div>
-                <div className="py-5">
-                    {currentTravels.map((travel) => (
-                    <div key={travel.id} className="flex items-center justify-between py-[16.5px]">
-                        <div className="w-1/3 text-center">
-                            {travel.dates}
+        <div className='mx-4 mt-16'>
+            <div className='text-3xl font-bold mb-5'>내 여행 목록</div>
+            <div className='rounded-md bg-brightgrey'>
+                <div className='border-b border-b-lightgrey py-5'>
+                    <div className='flex justify-between'>
+                        <div className='w-1/3 text-center'>
+                            <p>일정</p>
                         </div>
-                        <div className="w-1/3 text-center">
-                            {travel.places}
+                        <div className='w-1/3 text-center'>
+                            <p>장소</p>
                         </div>
-                        <div className="flex w-1/3 justify-center">
-                            <RoundBtn label="상세보기" color="bg-lightgrey" />
-                            <RoundBtn label="수정하기" color="bg-lightgrey" />
+                        <div className='w-1/3 text-center'>
+                            <p>관리하기</p>
                         </div>
                     </div>
+                </div>
+                <div className='py-5'>
+                    {datas.map((data: Props) => (
+                        <div
+                            key={data.id}
+                            className='flex items-center justify-between py-[16.5px]'
+                        >
+                            <div className='w-1/3 text-center'>
+                                {data.dates}
+                            </div>
+                            <div className='w-1/3 text-center'>
+                                {data.places}
+                            </div>
+                            <div className='flex w-1/3 justify-center'>
+                                <RoundBtn
+                                    label='상세보기'
+                                    color='bg-lightgrey'
+                                />
+                                <RoundBtn
+                                    label='수정하기'
+                                    color='bg-lightgrey'
+                                />
+                            </div>
+                        </div>
                     ))}
                 </div>
             </div>
-            <div className="flex justify-center m-8 text-grey">
-                <button className="mx-4 px-2" onClick={handlePrevClick} disabled={currentPage === 1}>
-                    &lt;
-                </button>
-                {currentPage}
-                <button className="mx-4 px-2" onClick={handleNextClick} disabled={indexOfLastTravel >= travels.length}>
-                    &gt;
-                </button>
-            </div>
+            <Pagination
+                totalPages={totalPages}
+                current={current}
+                setCurrent={setCurrent}
+            />
         </div>
-    )
+    );
 }
 
 export default MyTravel;
