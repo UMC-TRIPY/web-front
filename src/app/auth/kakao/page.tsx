c2d70073a2f0e2efce3f083338544b11fff8e1d3
rbgksqkr <rbgks1937@naver.com>
2023-08-12 17:26:21 +0900

    Feat: #104 - access token 발급 성공 시 login 상태 유지

--- src/app/auth/kakao/page.tsx
+++ src/app/auth/kakao/page.tsx
@@ -1,22 +1,26 @@
 'use client';
 
 import { getKakaoAccessToken } from '@/apis/user/login';
+import { isLoggedInState } from '@/states/user';
 import { splitAuthCode } from '@/utils/oauth';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import { useSetRecoilState } from 'recoil';
 
 const KakaoOAuth = () => {
     const router = useRouter();
     const params = useSearchParams();
     const code = params.get('code');
+    const setIsLoggedIn = useSetRecoilState(isLoggedInState);
 
     useEffect(() => {
         if (code)
             getKakaoAccessToken().then(() => {
                 console.log('code: ', code);
+                setIsLoggedIn(true);
                 router.push('/');
             });
-    }, [code, router]);
+    }, [code, router, setIsLoggedIn]);
     return <div></div>;
 };
 
