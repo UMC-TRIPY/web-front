2b94e8d304436a7cf1b3c099b0a07ff9089249aa
rbgksqkr <rbgks1937@naver.com>
2023-08-15 17:04:15 +0900

    Feat: #113 - 파일 업로드 기능 구현

--- src/components/mypage/profilePic.tsx
+++ src/components/mypage/profilePic.tsx
@@ -1,12 +1,46 @@
-export default function ProfilePic () {
+import { useRef, useState } from 'react';
+
+export default function ProfilePic() {
+    const fileRef = useRef<HTMLInputElement | null>(null);
+    const [imageSrc, setImageSrc]: any = useState('/images/profilePicLg.svg');
+
+    const handleClickProfile = () => {
+        if (fileRef.current) fileRef.current.click();
+    };
+
+    const handleChangeFile = (e: any) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+
+        return new Promise<void>((resolve) => {
+            reader.onload = () => {
+                setImageSrc(reader.result || null); // 파일의 컨텐츠
+                resolve();
+            };
+        });
+    };
+
     return (
-        <div className="w-auto h-80 flex justify-center items-center">
-            <img src="/images/profilePicLg.svg" alt="프로필 사진" />
-            <img 
-                src="/images/changePic.svg" 
-                alt="프로필 사진 변경" 
-                className="relative top-8 right-8 z-10"
+        <div className='w-auto h-80 flex justify-center items-center'>
+            <input
+                ref={fileRef}
+                type='file'
+                id='fileUpload'
+                style={{ display: 'none' }}
+                onChange={handleChangeFile}
+            />
+            <img
+                src={imageSrc}
+                alt='프로필 사진'
+                width={'20%'}
+                onClick={handleClickProfile}
+            />
+            <img
+                src='/images/changePic.svg'
+                alt='프로필 사진 변경'
+                className='relative top-8 right-8 z-10'
             />
         </div>
-    )
+    );
 }
