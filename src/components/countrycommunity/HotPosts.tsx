26da1eba2cd5bcadd786e67fea9927216c705d54
gs0428 <lider28@naver.com>
2023-08-23 14:40:42 +0900

    Chore : 충돌 해결

--- src/components/countrycommunity/HotPosts.tsx
+++ src/components/countrycommunity/HotPosts.tsx
@@ -1,18 +1,12 @@
 import PostLists from '../maincommunity/PostLists';
 
-
-export default function HotPosts() {
+export default function HotPosts({ country }: { country: string }) {
     const posts: [number, string, string, string, string][] = [
         [50, '오사카', '8월 오사카 여행 후기', '3', '10'],
         [51, '도쿄', '도쿄 맛집 추천해주세요!', '1', '15'],
         [52, '일본전체', '메이드 카페 후기(모에모에뀽)', '50', '127'],
-        [53,
-            '후쿠오카',
-            '후쿠오카 비행기 값',
-            '2',
-            '150'
-        ],
-        [54, '삿포로', '삿포로 여행 추천합니다', '10', '200'],
+        [53, '후쿠오카', '후쿠오카 비행기 값', '2', '150'],
+        [54, '삿포로', '삿포로 여행 추천합니다', '10', '200']
     ];
 
     return (
