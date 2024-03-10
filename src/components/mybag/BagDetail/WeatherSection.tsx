7ea79f94de0a594a5c09e39f5d3fb1ad19a8ffb7
gs0428 <lider28@naver.com>
2024-03-10 16:01:20 +0900

    refactor: 가방 디테일 날씨 부분 리팩토링

--- src/components/mybag/BagDetail/WeatherSection.tsx
+++ src/components/mybag/BagDetail/WeatherSection.tsx
@@ -1,30 +1,35 @@
+import { IoIosArrowDown } from 'react-icons/io';
 import { TiWeatherStormy } from 'react-icons/ti';
+import { SlLocationPin } from 'react-icons/sl';
 
 const WeatherSection = () => {
     return (
-        <div className='h-2/5 bg-brightgrey'>
-            <div className='flex h-1/5 items-center text-2xl font-bold pl-8'>
-                날씨
-            </div>
-            <div className='flex gap-8 h-1/5 justify-center items-center m-auto'>
-                <div className='flex gap-2'>
-                    <div>Day 1</div>
-                    <div>6월 30일</div>
+        <div className='bg-brightgrey p-5'>
+            <h1 className='text-2xl font-bold'>날씨</h1>
+            <div className='flex justify-center gap-16 mt-6 mb-10'>
+                <div className='flex text-xs items-center gap-3'>
+                    <span className='text-dark-black'>Day 1</span>
+                    <div className='flex gap-2 items-center'>
+                        <span className='font-bold'>6월 30일</span>
+                        <IoIosArrowDown />
+                    </div>
                 </div>
-                <div className='flex gap-2'>
-                    <div>마커</div>
-                    <div>부산, 해운대</div>
-                    <div className='text-grey'>대한민국</div>
+                <div className='flex items-center gap-2'>
+                    <SlLocationPin size={16} />
+                    <span className='text-xs text-dark-black'>
+                        부산, 해운대
+                    </span>
+                    <span className='text-xs text-grey'>대한민국</span>
                 </div>
             </div>
-            <div className='flex h-3/5 justify-center items-center gap-12'>
-                <TiWeatherStormy size={100} />
-                <div className='flex flex-col gap-2'>
-                    <div className='text-4xl'>16°</div>
-                    <div className='flex gap-4'>
-                        <div className='font-bold'>번개와 비구름</div>
-                        <div>23°</div>
-                        <div>12°</div>
+            <div className='flex justify-center items-center gap-10'>
+                <TiWeatherStormy size={90} />
+                <div className='flex flex-col gap-3'>
+                    <span className='text-[32px]'>16°</span>
+                    <div className='flex gap-3'>
+                        <span className='font-bold'>번개와 비구름</span>
+                        <span>23°</span>
+                        <span>12°</span>
                     </div>
                 </div>
             </div>
